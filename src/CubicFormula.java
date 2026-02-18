a33f3bb09b3a2d83389e6baf8671a50be53d7664
benjy <bccall666@gmail.com>
2026-02-17 23:09:05 -0500

    added the calculations for the first root

--- src/CubicFormula.java
+++ src/CubicFormula.java
@@ -12,7 +12,8 @@ public class CubicFormula {
         double c = keyboard.nextDouble();
         System.out.println("Please enter a nonzero d value:");
         double d = keyboard.nextDouble();
-        double discriminant = b*b*c*c - 4*a*Math.pow(c, 3) - 4*Math.pow(b, 3)*d - 27*a*a*d*d + 18*a*b*c*d;
+        double discriminant = b * b * c * c - 4 * a * Math.pow(c, 3) - 4 * Math.pow(b, 3) * -27 * a * a * d * d
+                + 18 * a * b * c * d;
         keyboard.close();
 
         if (discriminant > 0) {
@@ -22,5 +23,11 @@ public class CubicFormula {
         } else {
             System.out.println("There is 1 real root and 2 complex conjugate roots");
         }
+
+        double root1 = -(b / (3 * a)) - (1 / (3 * a)
+                * Math.cbrt((1 / 2) * (2 * b * b * b - 9 * a * b * c + 27 * a * a * d
+                        + Math.sqrt(Math.pow((2 * b * b * b - 9 * a * b * c + 27 * a * a * d), 2)
+                                - 4 * Math.pow((b * b - 3 * a * c), 3)))));
+        System.out.println(root1);
     }
 }
