b9a0f8c5aaf471ff55b55ee6834d6307bef41b52
Chen <343137386@tdsb.ca>
2026-02-18 10:54:19 -0500

    added branch to test other methods

--- src/CubicFormula.java
+++ src/CubicFormula.java
@@ -13,21 +13,24 @@ public class CubicFormula {
         System.out.println("Please enter a nonzero d value:");
         double d = keyboard.nextDouble();
         keyboard.close();
-        double discriminant = 18 * a * b * c * d - 4 * b * b * b * d + b * b * c * c - 4 * a * c * c * c - 27 * a * a * d * d;
-        double var1 = (b * c) / (6 * a * a) - d / (2 * a) - (b * b * b) / (27 * a * a * a);
-        double var2 = Math.pow(c / (3 * a) - (b * b) / (9 * a * a), 3);
-        double root1 = Math.cbrt(var1 + Math.sqrt(Math.pow(var1, 2) + var2))
-                + Math.cbrt(var1 - Math.sqrt(Math.pow(var1, 2) + var2)) - b / (3 * a);
+        
+        /* double p = (3 * a * c - b * b) / (3 * a * a);
+        double q = (2 * b * b - 9 * a * b * c + 27 * a * a * d) / (27 * a * a * a);
 
-        if (discriminant > 0) {
-            System.out.println("There is 3 distinct real roots");
-        } else if (discriminant == 0) {
-            System.out.println("All roots are equal, and at least 2 are repeated");
+        double depressed_discriminant = 4 * p * p * p + 27 * q * q * q;
+        if (p == 0) {
+            System.out.println("There is one triple root at x = 0");
+        } else if (depressed_discriminant == 0) {
+            double root1 = (3 * q / p) - b / 3 * a;
+            double root2 = -(3 * q / 2 * p) - b / 3 * a;
+            System.out.println("There is a root at " + root1 + ", and a repeated root at " + root2);
+        } */
+        
+        
+        if (-(b) == 3*a) {
+            System.out.println("There is a triple root at " + -(b/(3*a)));
         } else {
-            System.out.println("There is 1 real root and 2 complex conjugate roots");
+            double root1 = (9*a*d - b*c) / (a * (b*b - 3*a*c));
         }
-
-        System.out.println(root1);
-        System.out.println(discriminant);
     }
 }
