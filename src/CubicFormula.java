743614a9d562a17bfd1fda6540e1962f8a6db0e8
benjy <bccall666@gmail.com>
2026-02-19 19:33:44 -0500

    Merged branches, fixed up roots for when discriminant = 0

--- src/CubicFormula.java
+++ src/CubicFormula.java
@@ -13,27 +13,31 @@ public class CubicFormula {
         System.out.println("Please enter a nonzero d value:");
         double d = keyboard.nextDouble();
         keyboard.close();
-        
-        /* double p = (3 * a * c - b * b) / (3 * a * a);
-        double q = (2 * b * b - 9 * a * b * c + 27 * a * a * d) / (27 * a * a * a);
 
-        double depressed_discriminant = 4 * p * p * p + 27 * q * q * q;
-        if (p == 0) {
-            System.out.println("There is one triple root at x = 0");
-        } else if (depressed_discriminant == 0) {
-            double root1 = (3 * q / p) - b / 3 * a;
-            double root2 = -(3 * q / 2 * p) - b / 3 * a;
-            System.out.println("There is a root at " + root1 + ", and a repeated root at " + root2);
-        } */
-        
-        
-        if (-(b) == 3*a) {
-            System.out.println("There is a triple root at " + -(b/(3*a)));
+        double discriminant = 18 * a * b * c * d - 4 * b * b * b * d + b * b * c * c - 4 * a * c * c * c
+                - 27 * a * a * d * d;
+
+        double root1;
+        double root2;
+
+        if (discriminant > 0) {
+            System.out.println("There is 3 distinct real roots");
+        } else if (discriminant == 0) {
+            if (b * b == 3 * a * c) {
+                System.out.println("There is a triple root at " + -(b / (3 * a)));
+            } else {
+                root1 = (9 * a * d - b * c) / (2 * (b * b - 3 * a * c));
+                root2 = (4 * a * b * c - 9 * a * a * d - b * b * b) / (a * (b * b - 3 * a * c));
+                System.out.println("Roots: " + root1 + ", " + root2);
+            }
         } else {
-            double doubleRoot = (9*a*d - b*c) / (a * (b*b - 3*a*c));
-            double simpleRoot = (4*a*b*c - 9*a*a*d - b*b*b) / a * (b*b - 3*a*c);
-            System.out.println(doubleRoot);
-            System.out.println(simpleRoot);
+            double var1 = (b * c) / (6 * a * a) - d / (2 * a) - (b * b * b) / (27 * a * a * a);
+            double var2 = Math.pow(c / (3 * a) - (b * b) / (9 * a * a), 3);
+            root1 = Math.cbrt(var1 + Math.sqrt(Math.pow(var1, 2) + var2))
+                    + Math.cbrt(var1 - Math.sqrt(Math.pow(var1, 2) + var2)) - b / (3 * a);
+            System.out.println("Root: " + root1);
+            System.out.println("As well as 2 complex conjugate roots");
         }
+
     }
 }
